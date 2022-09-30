841359a50ad041d6ab8f23ef12154d1b5d4f9d60
Pablo Motta <pmotta.dev@gmail.com>
2022-09-29 18:02:16 -0700

    updated server file with graphql route

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
-import 'dotenv/config'
-import express from 'express'
-// import {graphqlHTTP} from "express-graphql"
-import cors from 'cors'
+import 'dotenv/config';
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { schema } from './Schema';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 5005;
 
 const main = async () => {
-    const app = express()
-    app.use(cors())
-    app.use(express.json())
-    // app.use('/graphql', graphqlHTTP({
-    //     schema, 
-    //     graphiql:true, 
-    // }))
-    app.listen(PORT, ()=> {
-        console.log(`Server started on port ${PORT} 🚀🌎`)
-    })
-}
+	const app = express();
+	app.use(cors());
+	app.use(express.json());
+	app.use(
+		'/graphql',
+		graphqlHTTP({
+			schema,
+			graphiql: true,
+		})
+	);
+	app.listen(PORT, () => {
+		console.log(`Server started on port ${PORT} 🚀🌎`);
+	});
+};
 
 main().catch((error) => {
-    console.log(error)
-})
+	console.log(error);
+});
