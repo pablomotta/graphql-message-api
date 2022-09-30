e754140ed2662b3877838514c1079f996554a714
Pablo Motta <pmotta.dev@gmail.com>
2022-09-29 17:04:17 -0700

    WIP - server setup

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+import express from 'express'
+// import {graphqlHTTP} from "express-graphql"
+import cors from 'cors'
+
+const PORT = process.env.PORT || 5005;
+
+const main = async () => {
+    const app = express()
+    app.use(cors())
+    app.use(express.json())
+    // app.use('/graphql', graphqlHTTP({
+    //     schema, 
+    //     graphiql:true, 
+    // }))
+    app.listen(PORT, ()=> {
+        console.log(`Server started on port ${PORT} 🚀🌎`)
+    })
+}
+
+main().catch((error) => {
+    console.log(error)
+})
