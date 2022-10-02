2f65340501a5a1a0de4bea865fb57fa03f5b7a8b
Pablo Motta <pmotta.dev@gmail.com>
2022-10-02 15:31:51 -0700

    sample data

--- src/Schema/data.ts
+++ src/Schema/data.ts
@@ -1,18 +1,21 @@
 export interface IData {
-    id:number,
-    message: string
-    mentions?: string[] |null 
-    emoticons?: string[] |null 
-    links?: string[] |null 
+	id: number;
+	message: string;
+	mentions?: string[] | null;
+	emoticons?: string[] | null;
+	links?: {
+		url?: string | null;
+		title?: string | null;
+	}[];
 }
 
-export const data:IData[] = [
+export const data: IData[] = [
 	{
 		id: 1,
-		message: 'This is a sample message (smile) @johnny - https://pablomotta.github.io/',
+		message:
+			'This is a sample message (smile) @johnny - https://pablomotta.github.io/',
 		mentions: ['johnny'],
 		emoticons: ['smile'],
-		links: ['https://pablomotta.github.io/'],
+		links: [{ url: 'https://pablomotta.github.io/', title: 'Pablo Motta' }],
 	},
 ];
-
